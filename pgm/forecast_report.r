197aef8804ce94b964d57f40e32e922a11fa31dc
DataEconomyLab <dataeconomylab@gmail.com>
2021-05-14 08:51:09 +0900

    UPDATE

--- pgm/forecast_report.r
+++ pgm/forecast_report.r
@@ -21,23 +21,29 @@ pacf(diff(log(forecast_report[,1])), main="로그 계열 부분상관도표")
 acf(diff(log(forecast_report[,1]), 4), main="로그차분 계열 상관도표")
 pacf(diff(log(forecast_report[,1]), 4), main="로그차분 계열 부분상관도표")
 
-#################################################################################################################
+############################################################################################
+setwd("C:/Users/ljyjj/Documents/GitHub/forecast/data")
+forecast_report <- ts(read.csv("forecast_report.csv", header=TRUE), start=1982, frequency=4)
+
+library(tseries)
+gdp <- forecast_report[,1]
+adf.test(log(gdp))
 
 library(forecast)
-plot(diff(log(forecast_report[,1])), ylab="분기민간소비GDP(조 원)", xlab="연도", col="black")
-acf(diff(log(forecast_report[,1])), main="로그 계열 상관도표")
-pacf(diff(log(forecast_report[,1], 4)), main="로그 계열 부분상관도표")
-gdp_fit = arima(log(forecast_report[,1]))
+par(mfrow=c(1,1))
+acf(diff(log(gdp),4))
+pacf(diff(log(gdp),4))
 
 # ARIMA 모형의 추정
-report_fit = arima(log(forecast_report[,1]), order=c(0,1,0))
+auto.arima(log(gdp))
+report_fit = arima(log(gdp), order=c(0,1,1), seasonal=c(0,1,1))
 summary(report_fit)
 
 # ARIMA 모형의 과대적합 검토
-report_fit1 = arima(log(forecast_report[,1]), order=c(1,1,1))
+report_fit1 = arima(log(gdp), order=c(1,1,1), seasonal=c(0,1,1))
 summary(report_fit1)
-report_fit2 = arima(log(forecast_report[,1]), order=c(0,1,2))
-summary(report_fit1)
+report_fit2 = arima(log(gdp), order=c(0,1,2), seasonal=c(0,1,1))
+summary(report_fit2)
 
 # ARIMA 모형의 진단
 tsdiag(report_fit)
